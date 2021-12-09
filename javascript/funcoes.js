9f7774ff9ee6b005130918650b10b48faaab8e82
Cassio Szerniak <cassioszerniak@gmail.com>
2021-12-09 18:35:42 -0300

    alterando src nas funcoes javascript

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,11 +1,11 @@
 
 /* abaixo variáveis para puxar as fotos*/
-var oculos = src="_imagens/glass-oculos-preto-peq.png";
-var home = src="_imagens/home.png";
-var espec = src="_imagens/especificacoes.png";
-var fotos = src="_imagens/fotos.png";
-var multimidia = src="_imagens/multimidia.png";
-var contato = src="_imagens/contato.png";
+var oculos = src="imagens/glass-oculos-preto-peq.png";
+var home = src="imagens/home.png";
+var espec = src="imagens/especificacoes.png";
+var fotos = src="imagens/fotos.png";
+var multimidia = src="imagens/multimidia.png";
+var contato = src="imagens/contato.png";
 
 function mudaFoto(foto) {
 document.getElementById("icone").src = foto;
