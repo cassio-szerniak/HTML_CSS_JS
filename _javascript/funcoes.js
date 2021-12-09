c4c2a68633c673c96e9bbaf6fbbc30d40ac29259
Cassio Szerniak <cassioszerniak@gmail.com>
2021-12-09 17:42:45 -0300

    Subindo os aqruivos do projeto

---
+++ _javascript/funcoes.js
@@ -0,0 +1,13 @@
+
+/* abaixo variáveis para puxar as fotos*/
+var oculos = src="_imagens/glass-oculos-preto-peq.png";
+var home = src="_imagens/home.png";
+var espec = src="_imagens/especificacoes.png";
+var fotos = src="_imagens/fotos.png";
+var multimidia = src="_imagens/multimidia.png";
+var contato = src="_imagens/contato.png";
+
+function mudaFoto(foto) {
+document.getElementById("icone").src = foto;
+}
+
